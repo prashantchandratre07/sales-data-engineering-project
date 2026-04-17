6e546587ce494b6890f2bafa539b7fbd7acdf368
prashantchandratre07 <prashantchandratre07@gmail.com>
2026-04-17 11:14:54 +0530

    Added ETL scripts

--- scripts/extract.py
+++ scripts/extract.py
@@ -1,1 +1,11 @@
+import pandas as pd
 
+def extract_data():
+    # Read CSV file
+    df = pd.read_csv('data/sales.csv')
+    print("Data Loaded Sucessfully!\n")
+    print(df.head())
+    return df
+
+if __name__ == "__main__":
+    extract_data()
